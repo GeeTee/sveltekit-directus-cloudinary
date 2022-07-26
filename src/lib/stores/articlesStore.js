330ae3b6926ad08036b272e7131c43ec89bfc5e1
Gille Testard <g.testard@galites.net>
2022-07-26 12:36:16 +0200

    start advanced articles

---
+++ src/lib/stores/articlesStore.js
@@ -0,0 +1,37 @@
+import { writable } from 'svelte/store'
+
+const articles = writable([]);
+
+const customArticlesStore = {
+    subscribe: articles.subscribe,
+    setArticles: (itemsArray) => {
+        articles.set(itemsArray);
+    },
+    addArticle: (ArticleData) => {
+        const newArticle = {
+            ...ArticleData
+        }
+        articles.update(items => {
+            return [newArticle, ...items];
+        });
+    },
+    updatePost: (id, ArticleData) => {
+        articles.update((items) => {
+        const ArticleIdx = items.findIndex((item) => item.id === id);
+        const updatedArticle = {
+            ...items[ArticleIdx],
+            ...ArticleData,
+        };
+        const updatedArticles = [...items];
+        updatedArticles[ArticleIdx] = updatedArticle;
+        return updatedArticles;
+        });
+    },
+    deleteArticle: (id) => {
+        articles.update((items) => {
+        return items.filter((item) => item.id !== id);
+        });
+    }
+};
+
+export default customArticlesStore;
